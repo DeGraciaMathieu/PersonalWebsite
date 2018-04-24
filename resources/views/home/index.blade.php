b74b429296d2df19d33d2c5f116e64d576886464
DeGraciaMathieu <degracia.mathieu@gmail.com>
2018-04-24 13:27:07 +0200

    Add view home.index

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html >
+<head>
+    <meta charset="UTF-8">
+    <title>De Gracia Mathieu</title>
+    <script src="https://s.codepen.io/assets/libs/modernizr.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Raleway:500,400,300,200,100' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<canvas class="container" id="container" role="main"></canvas>
+<div class="content">
+    <h1 class="title">De Gracia Mathieu</h1>
+    <p class="desc">Developpeur Web #laravel</p>
+    <ul class="contact">
+        <li>
+            <a href="mailto:degracia.mathieu@gmail.com">
+                <i class="fa fa-envelope-open" aria-hidden="true"></i>
+            </a>
+        </li>
+        <li>
+            <a target="_top" href="https://twitter.com/degraciamathieu">
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+            </a>
+        </li>
+        <li>
+            <a target="_top" href="https://www.linkedin.com/in/mathieu-de-gracia-64984b12a">
+                <i class="fa fa-linkedin" aria-hidden="true"></i>
+            </a>
+        </li>
+        <li>
+            <a target="_top" href="https://blog.degracia-mathieu.fr">
+                <i class="fa fa-pencil" aria-hidden="true"></i>
+            </a>
+        </li>
+        <li>
+            <a target="_top" href="https://energie-production.fr">
+                energie-production.fr
+            </a>
+        </li>       
+    </ul>  
+</div>
+<div class="blur blurTop"><canvas class="canvas" id="blurCanvasTop"></canvas></div>
+<div class="blur blurBottom"><canvas width="1000px" height="1000px" class="canvas" id="blurCanvasBottom"></canvas></div>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/snap.svg/0.3.0/snap.svg-min.js'></script>
+<script src='https://use.fontawesome.com/077f6cf5eb.js'></script>
+<script src="js/index.js"></script>
+</body>
+</html>
